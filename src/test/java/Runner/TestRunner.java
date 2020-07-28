b89042c0f48f2399c0d6379ca90052df3c238843
Deepalee.Khare <deepalee.khare@misys.com>
2020-07-28 12:02:03 +0530

    Update

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,14 +1,15 @@
 package Runner;
 
+import com.hpe.alm.octane.OctaneCucumber;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+@RunWith(OctaneCucumber.class)
 @CucumberOptions(
  features = "C:\\Program Files (x86)\\Jenkins\\workspace\\Octane\\src\\test\\java\\features",
  glue={"StepDefinitions"},
- plugin = {"pretty", "junit:target/OctaneGherkinResults.xml"}
+ plugin = {"junit:target/OctaneGherkinResults.xml"}
  )
 
 public class TestRunner {
